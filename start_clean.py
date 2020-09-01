770d828c1c5f2359a79f5507fffe8d8b565d99c9
Douglas Leeder <douglas.leeder@gmail.com>
2020-09-01 14:19:19 +0100

    Command to start clean.

---
+++ start_clean.py
@@ -0,0 +1,29 @@
+#!/bin/env python
+
+import serial
+import sys
+import time
+
+
+if len(sys.argv) > 1:
+    PORT = sys.argv[1]
+else:
+    PORT="/dev/ttyUSB0"
+
+
+BAUD_RATE=115200
+ser = serial.Serial(PORT, baudrate=BAUD_RATE, timeout=0.5)
+
+import irobot.openinterface.commands 
+from irobot.openinterface import commands
+from irobot.openinterface.constants import RESPONSE_SIZES
+from irobot.openinterface.response_parsers import unsigned_byte_response
+# open a serial connection
+# assumed to be serial here
+# change mode
+ser.write(commands.start())
+ser.write(commands.set_mode_passive())
+
+ser.write(commands.clean())
+
+ser.write(commands.set_mode_passive())
